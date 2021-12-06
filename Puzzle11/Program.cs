f33812f41ed4845fb11dd427ed446686da13884d
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2021-12-06 09:56:00 +0000

    Solve puzzle 11.

---
+++ Puzzle11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Puzzle11
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var input = await AdventOfCode.GetInput(6);
+
+            var initialFish = ParseInput(input);
+
+            var finalFish = SimulateFishReproductionOverDays(initialFish, 80);
+            
+            Console.WriteLine($"Lantern fish: {finalFish.Count}.");
+        }
+
+        private static IReadOnlyList<int> ParseInput(string input)
+        {
+            return input.Split(',')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse)
+                .ToList();
+        }
+
+        private static IReadOnlyList<int> SimulateFishReproductionOverDays(IReadOnlyList<int> initialFish, int days)
+        {
+            var fish = initialFish.ToList();
+            for (var day = 0; day < days; day++)
+            {
+                var fishAtTheBeginningOfTheDay = fish.Count;
+                for (var i = 0; i < fishAtTheBeginningOfTheDay; i++)
+                {
+                    --fish[i];
+                    if (fish[i] < 0)
+                    {
+                        fish[i] = 6;
+                        fish.Add(8);
+                    }
+                }
+            }
+            return fish;
+        }
+    }
+}
