33f9f9a9dd18ca90551e32b8e0f1dc11c0a95c7c
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2021-12-07 15:19:17 +0000

    Solve puzzle 13.

---
+++ Puzzle13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Puzzle13
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var input = await AdventOfCode.GetInput(7);
+
+            var positions = ParseInput(input);
+
+            var fuelNeededToLineUp = CalculateFuelNeededToLineUp(positions);
+            
+            Console.WriteLine($"Fuel needed to line up: {fuelNeededToLineUp}.");
+        }
+
+        private static IReadOnlyList<int> ParseInput(string input)
+        {
+            return input.Split(',')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse)
+                .OrderBy(i => i)
+                .ToList();
+        }
+
+        private static int CalculateFuelNeededToLineUp(IReadOnlyList<int> positions)
+        {
+            return positions
+                .Prepend(positions.Max() - positions.Min())
+                .Select(position => CalculateFuelNeededToLineUp(position, positions))
+                .Min();
+        }
+
+        private static int CalculateFuelNeededToLineUp(int position, IReadOnlyList<int> positions)
+        {
+            return positions.Sum(i => Math.Abs(i - position));
+        }
+    }
+}
