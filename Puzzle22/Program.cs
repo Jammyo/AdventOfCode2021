3a3d2c964a536bca01025198febafff19c1c8372
Jamey Abdelnoor <jamey.abdelnoor@incrowdsports.com>
2021-12-12 16:39:18 +0000

    Move relevant code to the shared project. Solve puzzle 22.

---
+++ Puzzle22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared;
+using Shared.Octopus;
+
+namespace Puzzle22
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var input = await AdventOfCode.GetInput(11);
+
+            var octopusTable = Octopus.ParseInput(input);
+
+            var iterations = IterateUntilOctopusAreSynchronised(octopusTable);
+            
+            Console.WriteLine($"Total flashes: {iterations}.");
+        }
+
+        private static int IterateUntilOctopusAreSynchronised(int[][] octopusTable)
+        {
+            var totalOctopus = octopusTable.Sum(ints => ints.Length);
+            var flashingOctopus = 0;
+            var iterations = 0;
+            
+            do
+            {
+                ++iterations;
+                flashingOctopus = Octopus.SimulateOctopusGrowth(octopusTable);
+            } while (flashingOctopus < totalOctopus);
+
+            return iterations;
+        }
+    }
+}
